e01401b9b3f36fff5fe444f23b9d339ce355db26
Fernando Zuffo <fzuffo@gmail.com>
2020-02-13 14:47:09 -0400

    first commit

---
+++ backend/database/migrations/1503250034279_user.js
@@ -0,0 +1,35 @@
+'use strict'
+
+/** @type {import('@adonisjs/lucid/src/Schema')} */
+const Schema = use('Schema')
+
+class UserSchema extends Schema {
+  up() {
+    this.create('users', table => {
+      table.increments()
+      table
+        .string('username', 80)
+        .notNullable()
+        .unique()
+      table
+        .string('email', 254)
+        .notNullable()
+        .unique()
+      table.string('password', 60).notNullable()
+      table.string('name', 254)
+      table.boolean('consultant').defaultTo(false)
+      table.string('document')
+      table.string('phone')
+
+      table.string('token')
+      table.timestamp('token_created_at')
+      table.timestamps()
+    })
+  }
+
+  down() {
+    this.drop('users')
+  }
+}
+
+module.exports = UserSchema
---
+++ backend/start/routes.js
@@ -0,0 +1,25 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| Http routes are entry points to your web application. You can create
+| routes for different URLs and bind Controller actions to them.
+|
+| A complete guide on routing is available here.
+| http://adonisjs.com/docs/4.1/routing
+|
+*/
+
+const Route = use('Route')
+
+Route.post('users', 'UserController.store')
+Route.post('sessions', 'SessionController.store')
+
+Route.post('passwords', 'ForgotPasswordController.store')
+Route.put('passwords', 'ForgotPasswordController.update')
+
+Route.get('files/:id', 'FileController.show') //fix to get by namefile
+Route.post('files', 'FileController.store')
