18aa85a0f9721d289642ceaf9211cac1f6ca50d2
Fernando Zuffo <fzuffo@gmail.com>
2020-02-14 17:25:29 -0400

    jobs queue

--- backend/app/Models/Consulting.js
+++ backend/app/Models/Consulting.js
@@ -1,9 +1,15 @@
 'use strict'
 
-/** @type {typeof import('@adonisjs/lucid/src/Lucid/Model')} */
 const Model = use('Model')
 
 class Consulting extends Model {
+  static boot() {
+    super.boot()
+
+    this.addHook('afterCreate', 'ConsultingHook.sendNewFormMail')
+    // this.addHook('afterUpdate', 'ConsultingHook.sendMailChangeStatus')
+  }
+
   user() {
     return this.belongsTo('App/Models/User')
   }
