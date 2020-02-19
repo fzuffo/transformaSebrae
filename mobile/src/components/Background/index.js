70f2fcdfceb0ee99a3d443c06b072e0f4539655a
fzuffo <fzuffo@gmail.com>
2020-02-19 17:06:20 -0400

    deleted mobile folder - not in use

--- mobile/src/components/Background/index.js
+++
@@ -1,8 +0,0 @@
-import LinearGradient from 'react-native-linear-gradient';
-import styled from 'styled-components/native';
-
-export default styled(LinearGradient).attrs({
-  colors: ['#fff', '#fff'],
-})`
-  flex: 1;
-`;
